969fe0cd8c5dd370e3d69d71d67235c7880a528d
Mebarek Mohamed Akram <mohamedakrame88@gmail.com>
2025-04-03 20:43:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,68 @@
-from fastapi import FastAPI, Request, HTTPException, Depends
+from fastapi import FastAPI, Request, HTTPException, Depends, Form, UploadFile, File, status
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
-from dotenv import load_dotenv
-import os
+from pydantic import EmailStr
+from typing import Union, Annotated
 from contextlib import asynccontextmanager
 from init_db import createtables
 from auth import authentification
 from database import get_db
-from schema import Usercreate,userinlogin
-from sqlalchemy.orm  import session
+from schema import Usercreate, userinlogin
+from sqlalchemy.orm import session
+import gdown
+import tensorflow as tf
+import io
+from PIL import Image
+import numpy as np
+import os
+
+class_names = ['Normal', 'sick']
+templates = Jinja2Templates(directory="templates")
 
 @asynccontextmanager
-async def lifespan(app : FastAPI):
+async def lifespan(app: FastAPI):
+    # Startup code
     createtables()
-
-    yield print('db is up now')
-
-
-
+    
+    # Download and load model
+    file_id = "1-B3xH3-3xvC06WDfZdlpwvd3frUbVDBg"
+    url = f"https://drive.google.com/uc?id={file_id}&confirm=t"
+    output = "lasttry_model_new.h5"
+    gdown.download(url, output, quiet=False, fuzzy=True)  # Add fuzzy flag to handle confirmation
+    if not os.path.exists(output):
+        raise RuntimeError("Failed to download the model.")
+    model = tf.keras.models.load_model(output)
+    app.state.model = model  # Store model in app state
+    print("Model loaded successfully.")
+    
+    yield
+    
+    # Shutdown code (if needed)
+    print("Shutting down...")
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(router=authentification,tags=["auth"] , prefix="/auth")
+app.include_router(router=authentification, tags=["auth"], prefix="/auth")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
-
-
-
-
-
-
-templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-
 @app.post('/data/{text}')
 def do(text: str):
-     
     return f"{text} ,your code is delivered to backend and treated"
 
-
-    
-
-import os
-import uvicorn
-
 if __name__ == "__main__":
+    import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
-
 
