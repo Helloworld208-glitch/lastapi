a742a4b8129dd5a9ba6ac47bdb2efac925d8c017
Mebarek Mohamed Akram <mohamedakrame88@gmail.com>
2025-04-04 17:46:41 +0100

    Update useradd.py

--- kjhkjhhj/useradd.py
+++ kjhkjhhj/useradd.py
@@ -32,6 +32,7 @@ from reportlab.lib.utils import ImageReader
 from PIL import Image
 from fastapi import UploadFile
 from fastapi.responses import StreamingResponse
+from sendmail import send_email_with_pdf
 AUTH_PREFIX='Bearer ' 
 class_names = ['Normal', 'sick']
 class Adduser(Fatherclass):
