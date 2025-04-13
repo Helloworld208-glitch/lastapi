d64510894e4bcc4202be8cadf60d511f4de78707
Mebarek Mohamed Akram <mohamedakrame88@gmail.com>
2025-04-13 14:06:24 +0100

    Update usermanagement.py

--- kjhkjhhj/usermanagement.py
+++ kjhkjhhj/usermanagement.py
@@ -64,6 +64,20 @@ class usermanagement(Adduser):
         if not file.content_type.startswith("image/"):
             raise HTTPException(status_code=400, detail="Not an image file")
         return await self.callai( email=email,request=request,  file=file, authorization=authorization)
+
+    async def chk_pic2(self,request: Request,  file: UploadFile = File(...),  email: EmailStr = Body(...),  authorization: Annotated[Union[str, None], Header()] = None):
+        if not file.content_type.startswith("image/"):
+            raise HTTPException(status_code=400, detail="Not an image file")
+        return await self.callai2( email=email,request=request,  file=file, authorization=authorization)
+    def get_admin_app(self,authorization:Annotated[Union[str,None],Header()]=None):
+
+
+
+
+
+
+
+    
     def get_admin_app(self,authorization:Annotated[Union[str,None],Header()]=None):
        
         auth_exeption=HTTPException(status_code =status.HTTP_401_UNAUTHORIZED,detail='error')
