3bfb90dadf7ea59f39566b5a0af7f068d9f08c66
Mebarek Mohamed Akram <mohamedakrame88@gmail.com>
2025-04-13 18:03:32 +0100

    Update auth.py

--- kjhkjhhj/auth.py
+++ kjhkjhhj/auth.py
@@ -91,4 +91,25 @@ def signup(email:str,authorization:Annotated[Union[str,None],Header()]=None, ses
 
 
 
+@authentification.post("/adminuploadtouser2")
+async def auth(
+    request: Request,  
+    email: EmailStr = Form(...),
+    file: UploadFile = File(...),
+    authorization: Annotated[Union[str, None], Header()] = None,
+    session=Depends(get_db)
+):
+    if not file.content_type.startswith("image/"):
+        return {"error": "Invalid file type, only images are allowed!"}
+    
+    result = await usermanagement(session=session).chk_pic2(
+        request=request,  
+        file=file,
+        email=email,
+        authorization=authorization
+    )
+    return result
+
+
+
 
