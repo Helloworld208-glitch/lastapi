2510f29fc200169b96dae5df240ba227f30f63ab
Mebarek Mohamed Akram <mohamedakrame88@gmail.com>
2025-04-13 21:57:00 +0100

    Update useradd.py

--- kjhkjhhj/useradd.py
+++ kjhkjhhj/useradd.py
@@ -262,7 +262,7 @@ class Adduser(Fatherclass):
         raise auth_exeption
     payload = jwtclass.chk_token(token=authorization[len(AUTH_PREFIX):])
     if payload and payload['role'] == "admin":
-        await self.send_email_with_pdf(
+        await send_email_with_pdf(
         subject="SCI Analysis Results",
         body=f"Dear {name},\n\nPlease find your analysis  attached. Contact us for further assistance.\n\nBest regards,\nSCI Team",
         to=email,
