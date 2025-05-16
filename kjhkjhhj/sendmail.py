e65e2be1848f3c39c4358562a53516dfbaebe182
Mebarek Mohamed Akram <mohamedakrame88@gmail.com>
2025-05-16 16:46:46 +0100

    Update sendmail.py

--- kjhkjhhj/sendmail.py
+++ kjhkjhhj/sendmail.py
@@ -11,12 +11,12 @@ def send_email(subject: str, body: str, to: str):
     msg.set_content(body)
 
     smtp_server = 'smtp.gmail.com'
-    smtp_port = 465  # Using SSL for Gmail
+    smtp_port = 465  
     username = 'scistudyproject2025@gmail.com'
-    app_password = 'pcbe qgur kylj taua'  # Your provided app password
+    app_password = 'vyyo untb ktsh zdof'  
 
     with smtplib.SMTP_SSL(smtp_server, smtp_port) as smtp:
-        smtp.set_debuglevel(1)  # Enable debug output
+        smtp.set_debuglevel(1)  
         smtp.login(username, app_password)
         smtp.send_message(msg)
 
@@ -31,17 +31,17 @@ def send_email_with_pdf(subject: str, body: str, to: str, pdf_buffer: io.BytesIO
     msg['To'] = to
     msg.set_content(body)
 
-    # Read PDF data from buffer and attach it
+    
     pdf_data = pdf_buffer.read()
     msg.add_attachment(pdf_data, maintype='application', subtype='pdf', filename='results.pdf')
 
     smtp_server = 'smtp.gmail.com'
-    smtp_port = 465  # Using SSL for Gmail
+    smtp_port = 465  
     username = 'scistudyproject2025@gmail.com'
-    app_password = 'nuvj utya gdbt zasx'  # Your provided app password
+    app_password = 'vyyo untb ktsh zdof'  
 
     with smtplib.SMTP_SSL(smtp_server, smtp_port) as smtp:
-        smtp.set_debuglevel(1)  # Enable debug output
+        smtp.set_debuglevel(1) 
         smtp.login(username, app_password)
         smtp.send_message(msg)
     return "hello guys im working"    
