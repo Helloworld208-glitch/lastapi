e0be34b0c25ea8e966b791ae87b417ca541605e3
Mebarek Mohamed Akram <mohamedakrame88@gmail.com>
2025-05-17 12:02:09 +0100

    Update useradd.py

--- kjhkjhhj/useradd.py
+++ kjhkjhhj/useradd.py
@@ -309,7 +309,75 @@ class Adduser(Fatherclass):
          
 
 
-  
+  async def callai(
+    self,
+    email: EmailStr,
+    request: Request,  # Add request parameter
+    file: UploadFile = File(...),
+    authorization: Annotated[Union[str, None], Header()] = None):
+    auth_exeption = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='error'
+    )
+    if not authorization:
+        raise auth_exeption
+    if not authorization.startswith(AUTH_PREFIX):
+        raise auth_exeption
+    payload = jwtclass.chk_token(token=authorization[len(AUTH_PREFIX):])
+    if payload and payload['role'] == "admin":
+        await self.results(request, file,email)  
+        return "done"
+  async def callai2(
+    self,
+    email: EmailStr,
+    request: Request,  # Add request parameter
+    file: UploadFile = File(...),
+    authorization: Annotated[Union[str, None], Header()] = None):
+    auth_exeption = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='error'
+    )
+    if not authorization:
+        raise auth_exeption
+    if not authorization.startswith(AUTH_PREFIX):
+        raise auth_exeption
+    payload = jwtclass.chk_token(token=authorization[len(AUTH_PREFIX):])
+    if payload:
+        return await self.results2(request, file,email)async def callai(
+    self,
+    email: EmailStr,
+    request: Request,  # Add request parameter
+    file: UploadFile = File(...),
+    authorization: Annotated[Union[str, None], Header()] = None):
+    auth_exeption = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='error'
+    )
+    if not authorization:
+        raise auth_exeption
+    if not authorization.startswith(AUTH_PREFIX):
+        raise auth_exeption
+    payload = jwtclass.chk_token(token=authorization[len(AUTH_PREFIX):])
+    if payload and payload['role'] == "admin":
+        await self.results(request, file,email)  
+        return "done"
+  async def callai22(
+    self,
+    email: EmailStr,
+    request: Request,  # Add request parameter
+    file: UploadFile = File(...),
+    authorization: Annotated[Union[str, None], Header()] = None):
+    auth_exeption = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='error'
+    )
+    if not authorization:
+        raise auth_exeption
+    if not authorization.startswith(AUTH_PREFIX):
+        raise auth_exeption
+    payload = jwtclass.chk_token(token=authorization[len(AUTH_PREFIX):])
+    if payload:
+        return await self.results3(request, file,email)
   from reportlab.lib.pagesizes import letter
   async def results3(self, request: Request, file: UploadFile, email: EmailStr):
     # Read the uploaded file
