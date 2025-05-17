9e929f6030aa4c5578662bf2ef6d2d9c22913443
Mebarek Mohamed Akram <mohamedakrame88@gmail.com>
2025-05-17 17:48:05 +0100

    Update main.py

--- kjhkjhhj/main.py
+++ kjhkjhhj/main.py
@@ -18,7 +18,7 @@ import numpy as np
 import os
 
 class_names = ['Normal', 'sick']
-class_names2 = ['Pneumonia_Merged', 'Normal', 'Lung_Opacity']
+class_names2 = ['Lung_Opacity', 'Normal', 'Pneumonia_Merged']
 templates = Jinja2Templates(directory="templates")
 
 
